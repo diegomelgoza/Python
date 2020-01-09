2066122a997ba198a4cef20531f1c243c60dde99
diegomelgoza <59671693+diegomelgoza@users.noreply.github.com>
2020-01-08 17:09:59 -0800

    Greet Users
    Greet users using their input.

---
+++ greeter.py
@@ -0,0 +1,12 @@
+"""name = input("Please enter your name: ")
+print("Hello, " + name + "!")"""
+
+prompt = "If you tell us who you are, we can personalize the messages"
+prompt += " you see. \nWhat is your first name? "
+
+name = input(prompt)
+print("Hello, " + name + "!")
+
+age = input("How old are you? ")
+print(age)
+
